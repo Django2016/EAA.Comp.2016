97e3740b15dddab9d736897a3522b3017ce55132
John Burns <jzburns@gmail.com>
2016-10-07 10:56:27 +0100

    start of lab2 part 2 backup

---
+++ week3/scriptBackupLab2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#script to back the document directory
+
+rmdir backup
+# deletes the directory if it already exists 
+
+echo "the back up dir: $date +%c"
+
+
---
+++ week3/scriptParameterslab2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "this is the parameter lab for week 2"
+
+# Part 1
+echo "total number of arguments: $#"
+
+echo "argument 1: $1"
+echo "argument 2: $2"
+echo "argument 3: $3"
+
+echo "Is this is correct?"
+
+#Part 2
